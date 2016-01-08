cbe05a052e421651446696a97aa9adae3eab835a
rr1000 <rrichrs@gmail.com>
2016-01-07 16:11:23 -0800

    Reverting build dir to default

--- config.rb
+++ config.rb
@@ -5,8 +5,6 @@ set :css_dir, 'assets/css'
 set :js_dir, 'assets/js'
 set :images_dir, 'assets/img'
 
-set :build_dir, '../html'
-
 configure :build do
   activate :minify_css
   activate :minify_javascript
